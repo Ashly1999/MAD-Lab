48fab3c5b3e16f178804b1e0879b036ee7d12bf7
Ashly1999 <74659110+Ashly1999@users.noreply.github.com>
2022-02-21 15:48:30 +0530

    Register

--- Cycle 4/MainActivity.java
+++ Cycle 4/MainActivity.java
@@ -1,39 +1,42 @@
-package com.example.sjcet.calculator;
+package com.example.sjcet.register;
 
+
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText number1;
-    EditText number2;
-    Button btn;
-    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        number1=findViewById(R.id.num1);
-        number2=findViewById(R.id.num2);
-        result=findViewById(R.id.result);
-        btn=findViewById(R.id.add);
+    }
 
-        btn.setOnClickListener(new View.OnClickListener() {
+    public void register(View view) {
+        Intent i = new Intent(MainActivity.this, register.class);
+        startActivity(i);
+    }
 
-            public void onClick(View v) {
+    public void login(View view) {
+        EditText E1, E2;
+        E1 =(EditText) findViewById(R.id.eduname);
+        E2 = (EditText) findViewById(R.id.edpass);
 
+        if ((E1.getText().toString().equals("admin")) && (E2.getText().toString().equals("admin")))
+        {
+            Intent i = new Intent(MainActivity.this,home.class);
+            startActivity(i);
+        }
 
-                double num1 = Double.parseDouble(number1.getText().toString());
-                double num2 = Double.parseDouble(number2.getText().toString());
-                double sum = num1 + num2;
-// set it ot result textview
-                result.setText(Double.toString(sum));
-            }
-        });
+        else
+        {
+            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
